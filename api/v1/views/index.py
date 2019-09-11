36c68e7e21fe6742ef5c342f75493f4219983075
jeniffervp <737@holbertonschool.com>
2019-09-11 00:45:06 +0000

    [Save] To destroy the container and reclaim a new one

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,16 @@
 """ """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route("/status")
 def json_status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    clases = ["Amenity", "City", "Place", "Review", "State", "User"]
+    dicti = {}
+    for i in clases:
+        dicti.setdefault(i, storage.count(i))
+    return jsonify(dicti)
