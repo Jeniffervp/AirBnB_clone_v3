fc23850e7e6126a52830e07834cf7475e411422d
jeniffervp <737@holbertonschool.com>
2019-09-11 00:24:52 +0000

    [Feature] handle not found error message

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """   """
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -12,6 +12,11 @@ app.register_blueprint(app_views)
 def teardown(self):
     """ function to close storage """
     storage.close()
+
+@app.errorhandler(404)
+def pagenotfound(e):
+    """ handle error 404 """
+    return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, threaded=True)
