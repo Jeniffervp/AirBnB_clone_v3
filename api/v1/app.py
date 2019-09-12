2eb752ece8619f7b5c2c8865679bb1063f1558f9
jeniffervp <737@holbertonschool.com>
2019-09-12 00:55:48 +0000

    [Feature] States api

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
@@ -24,4 +24,4 @@ def pagenotfound(e):
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = os.environ.get('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=int(port), threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,81 +1,93 @@
 #!/usr/bin/python3
 """  """
-from flask import Blueprint, Flask, jsonify, abort()
-from models import storage
 from api.v1.views import app_views
+from flask import Blueprint, Flask, jsonify, abort, request
+from models import storage
+from models.state import State
+from pdb import set_trace
 import os
 import json
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
 
 
 def get_id(state_id, stat):
     """  """
     my_dict = []
 
-    for i in stat:
+    for i in stat.values():
         if state_id == i.id:
-            for keys, val in i.items():
-                my_dict.append({keys: val})
+            my_dict.append(i.to_dict())
     return my_dict
 
 
-@app.route('/states', methods=['GET'])
-@app.route('/states/<state_id>', methods=['GET'])
+@app_views.route("/states")
+@app_views.route("/states/<state_id>", methods=['GET'])
 def state_retrieve(state_id=None):
     """  """
+    my_dict = []
     if state_id is None:
-        return storage.all("State")
+        st = {}
+        st = storage.all("State")
+        for i in st.values():
+            my_dict.append(i.to_dict())
+        return jsonify(my_dict)
     stat = storage.all("State")
     my_dict = get_id(state_id, stat)
     if len(my_dict) == 0:
         abort(404)
-    return(my_dict)
+    return jsonify(my_dict[0])
 
 
-@app.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delet_state(state_id=None):
     """  """
-    my_dict = get_id(state_id, stat)
+    stat = storage.all("State")
+    my_dict = []
+    for i in stat.values():
+        if state_id == i.id:
+            my_dict.append(i)
+            break
     if len(my_dict) == 0:
         abort(404)
-    storage.delete(mydict[0])
-    return {}, 200
+    storage.delete(my_dict[0])
+    storage.save()
+    storage.close()
+    return jsonify({}), 200
 
 
-@app.route('/states/', methods=['POST'])
+@app_views.route('/states/', methods=['POST'])
 def body_request():
     """  """
-    req = request.json()
-    try:
-        json.loads(data)
-    except ValueError:
-        print("Not a JSON")
-        abort(404)
+    if not request.is_json:
+        print("No a JSON")
+        abort(400)
+    req = request.get_json()
     if "name" not in req.keys():
         print("Missing name")
-        abort(404)
-    return req, 201
+        abort(400)
+    new_state = State(**req)
+    storage.new(new_state)
+    storage.save()
+    storage.close()
+    return jsonify(new_state.to_dict()), 201
 
 
-@app.route('/states/<state_id>', methods=['PUT'])
+@app_views.route('/states/<state_id>', methods=['PUT'])
 def put_state(state_id=None):
     """ """
-
-    req = request.json()
-    try:
-        json.loads(data)
-    except ValueError:
-        print("Not a JSON")
-        abort(404)
-    if "name" not in req.keys():
-        print("Missing name")
-        abort(404)
-    return req, 200
-
-    my_dict = get_id(state_id, stat)
+    if not request.is_json:
+        print("No a JSON")
+        abort(400)
+    req = request.get_json()
+    stat = storage.all("State")
+    my_dict = []
+    for i in stat.values():
+        if state_id == i.id:
+            my_dict.append(i)
+            break
     if len(my_dict) == 0:
         abort(404)
-    storage.delete(mydict[0])
-    return {}, 200
+    for j, i in req.items():
+        setattr(my_dict[0], j, i)
+    storage.save()
+    storage.close()
+    return jsonify(my_dict[0].to_dict()), 200
