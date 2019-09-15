e3edfc85b093ea8eb8d5e8d8fbd95501be8be26c
JaiberS <758@holbertonschool.com>
2019-09-15 04:51:45 +0000

    [Fixes] Comments

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Placex objects that handles all default RestFul API actions """
+""" Place objects that handles all default RestFul API actions """
 from api.v1.views import app_views
 from flask import Blueprint, Flask, jsonify, abort, request
 from models import storage
